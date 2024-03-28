49b92fd8f73bbe1b746ee414f6d3a05c27c74f5a
aryansinh07 <93570149+aryansinh07@users.noreply.github.com>
2024-03-28 12:15:03 +0530

    api urls changed to vercel urls

--- frontend/src/utils/apiURL.js
+++ frontend/src/utils/apiURL.js
@@ -1,5 +1,5 @@
-export const API_URL_USER = "http://localhost:7000/api/v1/users" ;
-export const API_URL_BOOK = "http://localhost:7000/api/v1/books" ; 
-export const API_URL_CHAT = "http://localhost:7000/api/v1/chat" ; 
-export const API_URL_MESSAGE = "http://localhost:7000/api/v1/message" ; 
-export const API_URL_REVIEW = "http://localhost:7000/api/v1/review" ; 
+export const API_URL_USER = "https://turning-leaves-backend.vercel.app/api/v1/users" ;
+export const API_URL_BOOK = "https://turning-leaves-backend.vercel.app/api/v1/books" ; 
+export const API_URL_CHAT = "https://turning-leaves-backend.vercel.app/api/v1/chat" ; 
+export const API_URL_MESSAGE = "https://turning-leaves-backend.vercel.app/api/v1/message" ; 
+export const API_URL_REVIEW = "https://turning-leaves-backend.vercel.app/api/v1/review" ; 
