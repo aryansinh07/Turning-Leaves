13163bab0f6484f25b156ab7c664f117a90e96ce
aryansinh07 <93570149+aryansinh07@users.noreply.github.com>
2024-04-04 20:46:57 +0530

    Backend Changes

--- backend/config/dbconfig.js
+++ backend/config/dbconfig.js
@@ -3,7 +3,7 @@ const { appErr } = require("../utilis/appErr");
 
 const dbConfig = async (next) => {
     try {
-        await mongoose.connect(process.env.DatabaseUrl); 
+        await mongoose.connect("mongodb+srv://saryan72881:fKDnaLGm716R8eC6@cluster0.fszs9rb.mongodb.net/online-book-thrift-store?retryWrites=true&w=majority"); 
         console.log("Database connected Successfully"); 
     } catch (error) {
         console.log(error); 
